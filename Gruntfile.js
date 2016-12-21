f85bcb799eba87801ff9e4ca0ea2d091b4d169bf
Mike van Rooyen <m.vanrooyen@gmail.com>
2016-12-21 11:42:33 +0000

    Basic jshint configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,25 +13,7 @@ module.exports = function(grunt) {
       }
     },
     jshint : {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        },
-      },
-      //uses_defaults: ['dir1/**/*.js', 'dir2/**/*.js'],
-      with_overrides: {
-        options: {
-          curly: false,
-          undef: true,
-        },
-        files: {
-          src: ['js/*.js']
-        },
-      }
+        src: ['js/*.js'],
     },
   });
 
